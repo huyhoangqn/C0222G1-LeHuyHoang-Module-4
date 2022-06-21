96aa183e5f9b6c0e9759e1a336f89aaa58b1dc95
huyhoangqn <lehuyhoangpass1@gmail.com>
2022-06-22 05:17:15 +0700

    fix ss5

--- 5. Thao tác với CSDL và ORM/exercise/mp3/src/main/java/mp3/controller/Mp3Controller.java
+++ 5. Thao tác với CSDL và ORM/exercise/mp3/src/main/java/mp3/controller/Mp3Controller.java
@@ -9,59 +9,57 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.List;
-
 @Controller
 public class Mp3Controller {
     @Autowired
     private IMp3Service iMp3Service;
 
     @GetMapping
-    public String home(Model model){
+    public String home(Model model) {
 
-        model.addAttribute("mp3List",iMp3Service.findAll());
+        model.addAttribute("mp3List", iMp3Service.findAll());
         return "/list";
     }
 
     @GetMapping("/create")
-    public String create(Model model){
-        model.addAttribute("mp3",new Mp3());
+    public String create(Model model) {
+        model.addAttribute("mp3", new Mp3());
         return "/create";
     }
 
     @PostMapping("/create")
-    public String save(Mp3 mp3){
+    public String save(Mp3 mp3) {
         iMp3Service.create(mp3);
         return "redirect:/";
     }
 
     @GetMapping("{id}/edit")
-    public String edit(@PathVariable int id, Model model){
-        model.addAttribute("mp3",iMp3Service.findById(id));
+    public String edit(@PathVariable int id, Model model) {
+        model.addAttribute("mp3", iMp3Service.findById(id));
         return "/edit";
     }
 
     @PostMapping("/edit")
-    public String upgrade(Mp3 mp3){
+    public String upgrade(Mp3 mp3) {
         iMp3Service.update(mp3);
         return "redirect:/";
     }
 
     @GetMapping("{id}/delete")
-    public String delete(@PathVariable int id , Model model){
-        model.addAttribute("mp3",iMp3Service.findById(id));
+    public String delete(@PathVariable int id, Model model) {
+        model.addAttribute("mp3", iMp3Service.findById(id));
         return "/delete";
     }
 
     @PostMapping("/delete")
-    public String remove(Mp3 mp3){
+    public String remove(Mp3 mp3) {
         iMp3Service.delete(mp3.getId());
         return "redirect:/";
     }
 
     @GetMapping("/search")
-    public String search(String name,Model model){
-        model.addAttribute("mp3List",iMp3Service.search(name));
+    public String search(String name, Model model) {
+        model.addAttribute("mp3List", iMp3Service.search(name));
         return "/list";
     }
 }
