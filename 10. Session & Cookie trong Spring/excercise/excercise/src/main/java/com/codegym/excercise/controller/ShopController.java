cd4e32e842d2c9063c3e9360109b58ae3b3786ea
huyhoangqn <lehuyhoangpass1@gmail.com>
2022-06-29 10:53:27 +0700

    ss10up

--- 10. Session & Cookie trong Spring/excercise/excercise/src/main/java/com/codegym/excercise/controller/ShopController.java
+++ 10. Session & Cookie trong Spring/excercise/excercise/src/main/java/com/codegym/excercise/controller/ShopController.java
@@ -9,10 +9,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.Optional;
 
 @Controller
+@SessionAttributes("cart")
 public class ShopController {
     @Autowired
     private IProductService productService;
@@ -21,6 +23,11 @@ public class ShopController {
     public String showShop(Model model) {
         model.addAttribute("products", productService.findAll());
         return "shoplist";
+    }
+
+    @ModelAttribute("cart")
+    public Cart getCart(){
+        return new Cart();
     }
 
     @GetMapping("/view/{productId}")
